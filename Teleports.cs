ee3d86726fc72ff0fbaf4f00b269dc7d401e06d3
Dine <86594060+dinendashd@users.noreply.github.com>
2021-07-02 22:10:36 -0400

    Add files via upload

---
+++ Teleports.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Teleports : MonoBehaviour
+{
+    public Transform player;
+    public Transform startLoc;
+    public Transform Loc1;
+
+
+    public void TeleStart()
+    {
+        player.transform.position = startLoc.transform.position;    
+    }
+    public void TeleLoc1()
+    {
+        player.transform.position = Loc1.transform.position;
+    }
+}
